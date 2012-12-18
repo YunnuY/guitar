32c37df389b9a2581f2bb076a844ae07c6aca3fd
ming <feitian124@gmail.com>
2012-12-18 22:47:17 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guitar::Application.config.secret_token = 'c8e414378d55c4e13bdd9426fa9934fe9e43567b64288c83e9557148e9c2ac0c60323aa391a4ed448807781da4a1e9541db54e224e471b48afe9f1bf5949f46e'
