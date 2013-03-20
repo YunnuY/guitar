fb4bd36b493d7b30877d2221c0bd33943f68c248
feitian124 <feitian124@gmail.com>
2013-03-20 22:41:33 +0800

    add seed data for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 user.confirm!
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+
+user2 = User.create! :name => 'User2', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
 user2.confirm!
-puts 'New user created: ' << user2.name
 user.add_role :admin
 
-puts "setting sample episodes"
-episode1 = Episode.create! :name => "episode1", :notes => "notes1", :published_at => "2012-12-03", :seconds => 10, :description => "description1", :comments_count => 0, :youku => "www.youku.com"
-
-puts "setting sample comments"
-comment1 = Comment.create! :episode_id => 1, :content => "comment1 content", :name => "First User", :email => "email@email.com", :user_id =>
-1, :ancestry => "ancestry"
-
-
+10.times do |index|
+  episode = Episode.create! :name => "episode#{index}", 
+                            :notes => "notes#{index}", 
+                            :published_at => "2012-12-03", 
+                            :seconds => 10, 
+                            :description => "description1", 
+                            :comments_count => 0, 
+                            :youku => "www.youku.com"
+  puts "episode #{episode.id} created"
+  (2 + Random.rand(3)).times do |index2|
+    comment = Comment.create! :episode_id => episode.id, 
+                    :content => "comment content", 
+                    :name => user.name, 
+                    :email => user.email, 
+                    :user_id => user.id
+  puts "comment #{comment.id} for episode #{episode.id} created"
+  end
+end
 
 
