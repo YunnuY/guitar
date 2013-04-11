87dc8cc1b9e81e1ae1391ca01e74b305ce8f8914
tiger <tiger.infosys@gmail.com>
2013-04-11 23:15:52 +0800

    keep user on the same page after signing out is ok now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,15 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end 
+
+  # keep user on the same page after signing in
+  # def after_sign_in_path_for(resource)
+  # 	current_user_path
+  # end
+
+
+  # keep user on the same page after signing out
+  def after_sign_out_path_for(resource_or_scope)
+  	request.referrer
+  end
 end
